3f2bad2c3be1a796171b04f19125e5b012fdbb41
Borja Ponz <fbponz@protonmail.ch>
2020-10-29 19:09:42 +0100

    update script task 1

--- env/scripts/borja/tarea1.py
+++ env/scripts/borja/tarea1.py
@@ -12,6 +12,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sales = pd.read_csv('../../data/sales_dataset.csv', sep=',', decimal='.')
+features = pd.read_csv('../../data/features_dataset.csv', sep=',', decimal='.')
+stores = pd.read_csv('../../data/stores_dataset.csv', sep=',', decimal='.')
+
 #QC OK
 """
 Cuantitative Weekly Sales
@@ -46,6 +49,39 @@ plt.show()
 """
 Nominal Store Weekly_Sales
 """
+temp_value = sales.reset_index().groupby('Store').Weekly_Sales.sum()
+temp_value = temp_value.reset_index()
+stores['Total_Sales'] = temp_value['Weekly_Sales']
+del(temp_value)
+tsps_descript = stores.Total_Sales.describe()
+
+mean = tsps_descript[1]
+std = tsps_descript[2]
+count = tsps_descript[0]
+
+
+mean_substract_std = mean - std
+mean_add_std = mean + std
+
+stores.loc[(stores['Total_Sales']<mean_substract_std), "class_sales"] = "1: Low sales"
+stores.loc[((stores['Total_Sales']>=mean_substract_std)&(stores['Total_Sales']<mean_add_std)), "class_sales"] = "2: Average sales"
+stores.loc[(stores['Total_Sales']>=mean_add_std), "class_sales"] = "3: High sales"
+
+mytable = stores.groupby(['class_sales']).size()
+count=mytable.sum()
+mytable2= (mytable/count)*100
+
+
+bar_list = ['Low sales', 'Average sales', 'High sales']
+plt.bar(bar_list, mytable2, edgecolor="black", color='green')
+plt.ylabel('Percentage')
+plt.xlabel('range of sales')
+plt.title('Figure 3: Percentage distribution based on total sales of each store')
+plt.show()
+
+
+"""
+"""
 store_1 = sales[sales["Store"] == 1]
 
 total_sales = sales["Weekly_Sales"].sum()
@@ -61,11 +97,33 @@ for handler in num_total_dept:
 
 mytable2 = (total_dept_sales/total_sales)*100
 
-title = 'Figure 1. Percentage of weather situations'
-xLabel = 'Different weather situations'
-yLabel = 'Percentage'
 plt.bar(bar_list,mytable2)
-plt.xlabel("Departament")
-plt.ylabel("Percentage department sales of total sales")
-plt.title("Fig.2: Only Deparments with sales above 10% of total")
+plt.xlabel("Departament name")
+plt.ylabel("Percentage over total sales")
+plt.title("Figure 6: Deparments with sales above 10% on total sales")
 plt.show()
+
+"""
+Rate $ of sale per each m2
+
+stores['rate_dollars_per_m2'] = stores['Total_Sales']/stores['Size']
+tsps_descript = stores.rate_dollars_per_m2.describe()
+
+mean = tsps_descript[1]
+std = tsps_descript[2]
+count = tsps_descript[0]
+stores.loc[(stores['rate_dollars_per_m2']<mean_substract_std), "rate_profit"] = "1: Low ROI"
+stores.loc[((stores['rate_dollars_per_m2']>=mean_substract_std)&(stores['rate_dollars_per_m2']<mean_add_std)), "rate_profit"] = "2: Average ROI"
+stores.loc[(stores['rate_dollars_per_m2']>=mean_add_std), "rate_profit"] = "3: High ROI"
+
+mytable = stores.groupby(['rate_profit']).size()
+count=mytable.sum()
+mytable2= (mytable/count)*100
+
+
+bar_list = ['Low ROI', 'Average ROI', 'High ROI']
+plt.bar(bar_list, mytable2, edgecolor="black", color='green')
+plt.ylabel('Percentage')
+plt.xlabel('range of sales')
+plt.title('Figure 3: Percentage distribution based on total sales of each store')
+plt.show()"""
